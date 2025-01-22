1d368447cd8844c2bcd15948b6a6fd2acfa6126b
Ilya8788 <122901504+Ilya8788@users.noreply.github.com>
2025-01-22 21:41:56 +0200

    Added download pdf , donlades component as pdf ,works

--- src/componentsClient/CVtemp1.jsx
+++ src/componentsClient/CVtemp1.jsx
@@ -1,16 +1,47 @@
 import React from "react";
 import "./cv.css";
 import { useLocation } from "react-router-dom";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 const Form1 = () => {
   const location = useLocation();
   let { data } = location.state || {};
   console.log(data);
 
+  //pdf download
+  const printRef = React.useRef(null);
+
+  const handleDownloadPdf = async () => {
+    const element = printRef.current;
+    if (!element) {
+      return;
+    }
+
+    const canvas = await html2canvas(element, {
+      scale: 2,
+    });
+    const data = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF({
+      orientation: "portrait",
+      unit: "px",
+      format: "a4",
+    });
+
+    const imgProperties = pdf.getImageProperties(data);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+
+    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, "PNG", 0, 0, pdfWidth, pdfHeight);
+    pdf.save("examplepdf.pdf");
+  };
+
   return (
     <>
       <div className="center">
-        <div className="container11">
+        <div ref={printRef} className="container11">
           <div style={{ display: "flex" }}>
             {/*Header */}
             <div style={{ background: "grey", width: "50%" }}>
@@ -84,6 +115,9 @@ const Form1 = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="center" style={{ margin: "5px" }}>
+        <button onClick={handleDownloadPdf}>Download</button>
       </div>
     </>
   );
